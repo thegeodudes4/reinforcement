7d4cf384ac76bec50acc497a3c8d9cf6906a0025
chdavis0917 <chdavis0917@gmail.com>
2022-11-01 15:11:16 -0700

    made progress on CreateHouseholdModal

--- client/components/CreateHouseholdModal.tsx
+++ client/components/CreateHouseholdModal.tsx
@@ -1,6 +1,57 @@
-
+import { useEffect, useState } from 'react';
 
 const CreateHouseholdModal = () => {
+
+  const [isHouseholdSubmitted, setIsHouseholdSubmitted] = useState(false);
+  const [householdModal, setHouseholdModal] = useState(true);
+  const [memberArr, setMemberArr] = useState([]);
+
+  const memberElement: JSX.Element = (
+    <div className="input-container">
+    <label>Member Name </label>
+    <input type="text" name="member" required />
+  </div>
+  );
+
+  const handleSubmit = async (event: Event) :Promise<void> => {
+
+      //Prevent page reload
+      event.preventDefault();
+  
+      const { household, member } = document.forms[0];
+  
+      // document.forms[0] grabs the value of whatever text is in the household and member input fields
+  
+  
+      const userData = null;
+      //await createHousehold(household.value, member.value)
+  
+  
+      // Compare user info
+      if (userData) {
+        console.log("new household created!")
+        setIsHouseholdSubmitted(true);
+        setHouseholdModal(false);
+        return;
+      }
+
+      else {
+        renderErrorMessage();
+        // this will display an error message if not logged in.
+      }
+    };
+
+  const renderErrorMessage = () => {
+      <div className="error">Please log in to create household.</div>
+}
+
+
+  const addMember = () => {
+    console.log("What is memberElement?", memberElement);
+    console.log("What is typeof memberElement?", typeof memberElement);
+    setMemberArr(memberArr.push(memberElement));
+  };
+
   return (
     <div className="createHouseholdModal">
       <h1>Create Household Modal</h1>
@@ -9,19 +60,16 @@ const CreateHouseholdModal = () => {
         <div className="input-container">
           <label>Household Name </label>
           <input type="text" name="household"  required />
-          {renderErrorMessage("household")}
         </div>
-        <div className="input-container">
-          <label>Member Name </label>
-          <input type="text" name="pass" required />
-          {renderErrorMessage("pass")}
+        {memberElement}
+        {memberArr}
+        <div className="button-container">
+          <button onClick={addMember}>Add Member</button>
         </div>
         <div className="button-container">
           <input type="submit" />
         </div>
-        <div className="button-container">
-          <input type="button" value="Sign Up Here" onClick={() => handleClick()}/>
-        </div>
+        {renderErrorMessage()}
       </form>
     </div>
     </div>
--- client/containers/CreateHouseholdContainer.tsx
+++ client/containers/CreateHouseholdContainer.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 // import from child component...
 import CreateHouseholdModal from '../components/CreateHouseholdModal';
 
 const CreateHouseholdContainer = () => {
+  const [householdModal, setHouseholdModal] = useState(false);
+
+  const renderCreateHouseholdModal = () => {
+    if (householdModal === false) setHouseholdModal(true);
+  }
+
+  if (householdModal === false) {
   return (
     <div className="createHouseholdContainer">
-      <button>Create Household Container</button>
-      <CreateHouseholdModal />
+      <h1>Create Household Container</h1>
+      <button onClick={renderCreateHouseholdModal}>Create Household</button>
     </div>
   );
+  }
+  else return (
+    <div className="createHouseholdContainer">
+      <h1>Create Household Container</h1>
+    <CreateHouseholdModal />
+    </div>
+  )
 };
 
 export default CreateHouseholdContainer;
